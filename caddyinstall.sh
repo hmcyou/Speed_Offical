116b06e444521410e7692bde4947e680439fb0b3
NewCheung <41122937+NewCheung@users.noreply.github.com>
2020-10-07 21:59:36 +0800

    Update caddyinstall.sh

--- caddyinstall.sh
+++ caddyinstall.sh
@@ -1,33 +1,108 @@
-#Caddy 安装脚本，根据官方文档
-mkdir caddy && cd caddy
-mkdir -p /etc/caddy/
-touch /etc/caddy/Caddyfile
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 
-wget https://github.com/caddyserver/caddy/releases/download/v2.2.0/caddy_2.2.0_linux_amd64.tar.gz
-tar -xzvf caddy_2.2.0_linux_amd64.tar.gz
+Install_caddy(){
+	#Caddy 安装脚本，根据官方文档
+	`mkdir caddy && cd caddy`
+	`mkdir -p /etc/caddy/`
+	`touch /etc/caddy/Caddyfile`
 
-mv caddy /usr/bin/
+	`wget https://github.com/caddyserver/caddy/releases/download/v2.2.0/caddy_2.2.0_linux_amd64.tar.gz`
+	`tar -xzvf caddy_2.2.0_linux_amd64.tar.gz`
 
-groupadd --system caddy
+	`mv caddy /usr/bin/`
 
-useradd --system \
-    --gid caddy \
-    --create-home \
-    --home-dir /var/lib/caddy \
-    --shell /usr/sbin/nologin \
-    --comment "Caddy web server" \
-    caddy
-	
-wget https://github.com/caddyserver/dist/raw/master/init/caddy.service
+	`groupadd --system caddy`
 
-mv caddy.service /etc/systemd/system/
+	`useradd --system \
+		--gid caddy \
+		--create-home \
+		--home-dir /var/lib/caddy \
+		--shell /usr/sbin/nologin \
+		--comment "Caddy web server" \
+		caddy`
+		
+	`wget https://github.com/caddyserver/dist/raw/master/init/caddy.service`
 
-systemctl daemon-reload
+	`mv caddy.service /etc/systemd/system/`
 
-systemctl enable caddy
+	`systemctl daemon-reload`
 
-systemctl start caddy
+	`systemctl enable caddy`
 
-echo "安装完成，请编辑 /etc/caddy/Caddyfile 文件，完成后执行systemctl restart caddy && systemctl status caddy"
+	systemctl start caddy
 
-rm -rf caddy/ caddyinstall.sh
+	echo "安装完成，请编辑 /etc/caddy/Caddyfile 文件，完成后执行systemctl restart caddy && systemctl status caddy"
+
+	`rm -rf caddy/ caddy_2.2.0_linux_amd64.tar.gz`
+	Estart
+}
+
+Uninstall_caddy(){
+	`systemctl stop caddy`
+	`rm -rf /usr/bin/caddy /etc/systemd/system/caddy.service /etc/caddy/`
+	echo "卸载完成"
+	Estart
+}
+
+Start_caddy(){
+	`systemctl start caddy`
+	echo "caddy 已启动"
+	Estart
+}
+
+Stop_caddy(){
+	`systemctl stop caddy`
+	echo "caddy 已停止"
+	Estart
+}
+
+Restart_caddy(){
+	`systemctl restart caddy`
+	echo "caddy 重启成功"
+	Estart
+}
+
+Status_caddy(){
+	`systemctl status caddy`
+	Estart
+}
+
+Estart(){
+echo && echo -e "  caddy Debian/ubuntu 一键安装脚本
+  
+	1.安装 caddy
+	2.卸载 caddy
+	————————————————————————
+	3.启动 caddy
+	4.停止 caddy
+	5.重启 caddy
+	6.查看 caddy状态
+	————————————" && echo
+read -e -p " 请输入数字 [1-5]:" num
+case "$num" in
+	1)
+	Install_caddy
+	;;
+	2)
+	Uninstall_caddy
+	;;
+	3)
+	Start_caddy
+	;;
+	4)
+	Stop_caddy
+	;;
+	5)
+	Restart_caddy
+	;;
+	6)
+	Status_caddy
+	;;
+	*)
+	echo "请输入正确数字 [1-5]"
+	;;
+esac
+}
+Estart

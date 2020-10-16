d722b6e81255828b33f44d5680ea0cdc0aa52f58
NewCheung <41122937+NewCheung@users.noreply.github.com>
2020-10-16 12:53:15 +0800

    Update caddyinstall.sh

--- caddyinstall.sh
+++ caddyinstall.sh
@@ -7,14 +7,10 @@ Install_caddy(){
 	`mkdir caddy && cd caddy`
 	`mkdir -p /etc/caddy/`
 	`touch /etc/caddy/Caddyfile`
-
 	`wget https://github.com/caddyserver/caddy/releases/download/v2.2.0/caddy_2.2.0_linux_amd64.tar.gz`
 	`tar -xzvf caddy_2.2.0_linux_amd64.tar.gz`
-
 	`mv caddy /usr/bin/`
-
 	`groupadd --system caddy`
-
 	`useradd --system \
 		--gid caddy \
 		--create-home \
@@ -22,46 +18,44 @@ Install_caddy(){
 		--shell /usr/sbin/nologin \
 		--comment "Caddy web server" \
 		caddy`
-		
 	`wget https://github.com/caddyserver/dist/raw/master/init/caddy.service`
-
 	`mv caddy.service /etc/systemd/system/`
-
 	`systemctl daemon-reload`
-
 	`systemctl enable caddy`
-
-	systemctl start caddy
-
-	echo "安装完成，请编辑 /etc/caddy/Caddyfile 文件，完成后执行systemctl restart caddy && systemctl status caddy"
-
-	`rm -rf caddy/ caddy_2.2.0_linux_amd64.tar.gz`
+	`systemctl start caddy`
+	systemctl status caddy
 	Estart
+	echo "安装完成，请编辑 /etc/caddy/Caddyfile 文件，完成后执行systemctl restart caddy && systemctl status caddy"
+	`rm -rf caddy/ caddy_2.2.0_linux_amd64.tar.gz`
 }
 
 Uninstall_caddy(){
 	`systemctl stop caddy`
 	`rm -rf /usr/bin/caddy /etc/systemd/system/caddy.service /etc/caddy/`
-	echo "卸载完成"
 	Estart
+	echo "卸载完成"
 }
 
 Start_caddy(){
 	`systemctl start caddy`
-	echo "caddy 已启动"
 	Estart
+	echo "caddy 已启动"
 }
 
 Stop_caddy(){
 	`systemctl stop caddy`
-	echo "caddy 已停止"
 	Estart
+	echo "caddy 已停止"
 }
 
 Restart_caddy(){
 	`systemctl restart caddy`
-	echo "caddy 重启成功"
 	Estart
+	echo "caddy 重启成功"
+}
+
+Edit_caddy(){
+	nano /etc/caddy/Caddyfile
 }
 
 Estart(){
@@ -73,6 +67,8 @@ echo && echo -e "  caddy Debian/ubuntu 一键安装脚本
 	3.启动 caddy
 	4.停止 caddy
 	5.重启 caddy
+	————————————————————————
+	6.编辑配置文件
 	————————————" && echo
 read -e -p " 请输入数字 [1-5]:" num
 case "$num" in
@@ -90,6 +86,9 @@ case "$num" in
 	;;
 	5)
 	Restart_caddy
+	;;
+	6)
+	Edit_caddy
 	;;
 	*)
 	echo "请输入正确数字 [1-5]"

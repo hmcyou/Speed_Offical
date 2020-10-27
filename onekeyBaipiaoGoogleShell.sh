47076f677dc0476b4c029dcb7ec5825180308b85
NewCheung <41122937+NewCheung@users.noreply.github.com>
2020-10-27 13:34:05 +0800

    Create onekeyBaipiaoGoogleShell.sh

---
+++ onekeyBaipiaoGoogleShell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo root:123qwe |sudo chpasswd root
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+echo Port 22 >> /etc/ssh/sshd_config
+ervice ssh restart
+
+clear
+echo "***********白嫖成功！***********"
+echo "IP地址:"
+curl ip.sb
+echo "SSH端口号:6000  用户名:root  密码:123qwe"
